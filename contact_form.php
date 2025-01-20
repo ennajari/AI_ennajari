0ef4a16b9f7dbe0afbc6a9364cb2d67814abea71
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2025-01-20 22:02:49 +0100

    toast message

--- contact_form.php
+++ contact_form.php
@@ -1,24 +1,26 @@
 <?php
 
-$mail_to  = "abdellah.ennajari.23@ump.ac.ma";
+$mail_to = "abdellah.ennajari.23@ump.ac.ma";
 
-$name     = $_POST['name'];
-$email    = $_POST['email'];
-$phone    = $_POST['phone'];
-$message  = $_POST['message'];
+$name     = isset($_POST['name']) ? $_POST['name'] : '';
+$email    = isset($_POST['email']) ? $_POST['email'] : '';
+$phone    = isset($_POST['phone']) ? $_POST['phone'] : '';
+$message  = isset($_POST['message']) ? $_POST['message'] : '';
 
 $subject = "Contact Request From: " . $name;
 
-$body   = "New Message From: " . "$name " . "<$email>" . "\n\n";
-$body   .= "----------------" . "\n\n";
-$body   .= "Name: " . "$name" . "\n\n";
-$body   .= "Email: " . "$email" . "\n\n";
-$body   .= "Phone: " . "$phone" . "\n\n";
-$body   .= "Message: " . "\n\n" . "$message" . "\n\n";
+$body   = "New Message From: " . $name . " <" . $email . ">\n\n";
+$body   .= "----------------\n\n";
+$body   .= "Name: " . $name . "\n\n";
+$body   .= "Email: " . $email . "\n\n";
+$body   .= "Phone: " . $phone . "\n\n";
+$body   .= "Message: \n\n" . $message . "\n\n";
 
-$from  = "From: " . "$name " . "<$email>";
+$headers  = "From: " . $name . " <" . $email . ">\r\n";
+$headers .= "Reply-To: " . $email . "\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-if (mail($mail_to, $subject, $body, $from)) {
+if (mail($mail_to, $subject, $body, $headers)) {
   echo "success";
 } else {
   echo "error";
